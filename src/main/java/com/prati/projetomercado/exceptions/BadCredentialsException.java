3fd5b15cbb31220e3d990eea1e2d0cbd7e598e64
leonamsh <lpdmonteiro@gmail.com>
2025-10-19 09:53:05 -0300

    Fix:  BadRequestException não está retornando devidamente.
    Agora retorna:
    
    {
      "timestamp": "2025-10-19T01:13:56.137641924Z",
      "status": 400,
      "error": "Bad Request",
      "message": "A senha deve ter pelo menos 6 caracteres.",
      "path": "/auth/register"
    }
    
    Fix: Algumas exceptions não estão sendo tratadas devidamente
    
    Fix: Quando o usuário tenta cadastrar usando um email já cadastrado, a exception explode e não está sendo tratada
    
    Fix Typo

--- src/main/java/com/prati/projetomercado/exceptions/BadCredentialsException.java
+++ src/main/java/com/prati/projetomercado/exceptions/BadCredentialsException.java
@@ -8,16 +8,6 @@ import java.util.List;
 public class BadCredentialsException extends RuntimeException {
     private final List<FieldError> fieldErrors;
 
-    public BadCredentialsException(String message) {
-        super(message);
-        this.fieldErrors = null;
-    }
-
-    public BadCredentialsException(String message, List<FieldError> fieldErrors) {
-        super(message);
-        this.fieldErrors = fieldErrors;
-    }
-
     public BadCredentialsException(List<FieldError> fieldErrors) {
         super("Credenciais inválidas");
         this.fieldErrors = fieldErrors;
